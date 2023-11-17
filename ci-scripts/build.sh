c875536c293b309e5caf4206508d560dedea08ba
Shoaib Gauhar <sagauhar@yahoo.com>
2023-11-18 00:14:40 +1100

    Use command param

--- ci-scripts/build.sh
+++ ci-scripts/build.sh
@@ -24,9 +24,7 @@ docker run \
   -u root \
   -w /app \
   golang:1.12 \
-  apt-get update \ 
-  apt-get -y upgrade \ 
-  make build
+  -c "apt-get update && apt-get -y upgrade && make build"
 
 echo "--- $(date '+%H:%M:%S') :docker: Cleanup..."
 docker rm -vf ${BUILDKITE_PIPELINE_SLUG}.$$
